10a67593b700043f528700b0397dbbcc2830df09
Fabian Klopfer <fabian.klopfer@ieee.org>
2019-01-26 16:33:07 +0100

    started to impl codegen

---
+++ oberon0c/parser/CodeGen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fabian on 26.01.19.
+//
+
+#include "CodeGen.h"
---
+++ oberon0c/parser/CodeGen.h
@@ -0,0 +1,14 @@
+//
+// Created by fabian on 26.01.19.
+//
+
+#ifndef OBERON0C_CODEGEN_H
+#define OBERON0C_CODEGEN_H
+
+
+class CodeGen {
+
+};
+
+
+#endif //OBERON0C_CODEGEN_H
